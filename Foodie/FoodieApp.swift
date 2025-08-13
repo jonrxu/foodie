85a5196a44d64d784d4788998255fd7f500cc6a6
jonrxu <jonrxu@gmail.com>
2025-08-12 20:08:23 -0400

    Initial Commit

---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Jonathan Xu on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodieTests/FoodieTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieTests.swift
+//  FoodieTests
+//
+//  Created by Jonathan Xu on 8/12/25.
+//
+
+import Testing
+@testable import Foodie
+
+struct FoodieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
